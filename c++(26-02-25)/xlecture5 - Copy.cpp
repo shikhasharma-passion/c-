a15a83e17e348c2dfafef1e0ac7c5509b3be1ee3
Shikha Sharma <144619810+shikhasharma-passion@users.noreply.github.com>
2026-01-06 21:08:42 +0530

    Update xlecture5 - Copy.cpp

--- c++(26-02-25)/xlecture5 - Copy.cpp
+++ c++(26-02-25)/xlecture5 - Copy.cpp
@@ -1,28 +1,28 @@
 #include<iostream>
 using namespace std;
-// int numIsPrime(int n){
-//     bool isPrime = true;
-//     bool isNotPrime = false;
-//     for(int i=2;i<=n;i++){
-//         if(n%i==0){
-//             cout<<isNotPrime;
-//         }else{
-//             cout<<isPrime;
-//         } 
-//       }
-//       return isPrime;
-//     }
-//     int main(){
-//         cout <<numIsPrime(9)<<endl;
+int numIsPrime(int n){
+    bool isPrime = true;
+    bool isNotPrime = false;
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            cout<<isNotPrime;
+        }else{
+            cout<<isPrime;
+        } 
+      }
+      return isPrime;
+    }
+    int main(){
+        cout <<numIsPrime(9)<<endl;
 
-     int num(){
-        cout<<"hlo shikha\n";
-        return 3;
-     }
+     // int num(){
+     //    cout<<"hlo shikha\n";
+     //    return 3;
+     // }
 
-     int main(){
+     // int main(){
         
-        cout<<num<<endl;
+     //    cout<<num<<endl;
      
         return 0;
     }
